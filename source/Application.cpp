98b704053253fd10906be330ad04a617c36519d2
Bryan DeGrendel <bryan@degrendel.com>
2015-05-06 00:50:47 +0000

    Application now creates the window

--- source/Application.cpp
+++ source/Application.cpp
@@ -5,6 +5,12 @@
 
 
 #include "Application.hpp"
+#include "Window.hpp"
+
+const int DEFAULT_WINDOW_X = 100;
+const int DEFAULT_WINDOW_Y = 100;
+const int DEFAULT_WINDOW_WIDTH = 800;
+const int DEFAULT_WINDOW_HEIGHT = 800;
 
 Application::Application()
   : BApplication("application/com.degrendel.haiku-scientific-calculator"), window(NULL)
@@ -13,5 +19,8 @@ Application::Application()
 
 void Application::ReadyToRun()
 {
+  BRect frame(DEFAULT_WINDOW_X, DEFAULT_WINDOW_Y, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
+  window = new Window(frame);
+  window->Show();
 }
 
