27a5cbf9ab9b5b8a07cce813224780eba757d6a8
Bryan DeGrendel <bryan@degrendel.com>
2015-05-06 20:35:54 -0400

    Added first pass at a Go button

---
+++ include/Actions/Go.hpp
@@ -0,0 +1,17 @@
+// Haiku Scientific Calculator
+// Bryan DeGrendel (c) 2015
+//
+// See LICENSE for licensing information.
+
+#ifndef ___ACTION_GO_H___
+#define ___ACTION_GO_H___
+
+#include "Actions/Action.hpp"
+
+class Go : public Action
+{
+public:
+  Go();
+};
+
+#endif // ___ACTION_GO_H___
